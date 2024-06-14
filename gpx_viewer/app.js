68a81c8c1058e0ae46c819025a42e68bf959eeb0
harrytool <ivan1047@gmail.com>
2024-06-14 14:20:41 +0800

    Update app.js

--- gpx_viewer/app.js
+++ gpx_viewer/app.js
@@ -6,24 +6,32 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 18
 }).addTo(map);
 
-// 替換目前的事件監聽器
-var gpxUrl = 'gpx/routes.gpx';  // 替換成您的 GPX 檔案的 URL
+// 解析網址參數
+var params = new URLSearchParams(window.location.search);
+var gpxFile = params.get('file');
 
-fetch(gpxUrl)
-    .then(response => response.text())
-    .then(gpxData => {
-        // 使用 DOMParser 解析 GPX 檔案
-        var parser = new DOMParser();
-        var xmlDoc = parser.parseFromString(gpxData, 'application/xml');
+// 檢查是否有提供 GPX 文件的參數
+if (gpxFile) {
+    var gpxUrl = gpxFile;  // 使用網址參數中的 GPX 檔案的 URL
 
-        // 使用 toGeoJSON 將 GPX 轉換為 GeoJSON
-        var geojson = toGeoJSON.gpx(xmlDoc);
+    fetch(gpxUrl)
+        .then(response => response.text())
+        .then(gpxData => {
+            // 使用 DOMParser 解析 GPX 檔案
+            var parser = new DOMParser();
+            var xmlDoc = parser.parseFromString(gpxData, 'application/xml');
 
-        // 創建 GeoJSON 圖層並添加到地圖
-        var geojsonLayer = L.geoJSON(geojson);
-        geojsonLayer.addTo(map);
+            // 使用 toGeoJSON 將 GPX 轉換為 GeoJSON
+            var geojson = toGeoJSON.gpx(xmlDoc);
 
-        // 調整地圖視角以適應 GPX 軌跡
-        map.fitBounds(geojsonLayer.getBounds());
-    })
-    .catch(error => console.error('Error fetching the GPX file:', error));
+            // 創建 GeoJSON 圖層並添加到地圖
+            var geojsonLayer = L.geoJSON(geojson);
+            geojsonLayer.addTo(map);
+
+            // 調整地圖視角以適應 GPX 軌跡
+            map.fitBounds(geojsonLayer.getBounds());
+        })
+        .catch(error => console.error('Error fetching the GPX file:', error));
+} else {
+    console.error('GPX file parameter is missing in the URL.');
+}
